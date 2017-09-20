6ba0c58e35e1a98f64498850309f7ea550ded591
lost273 <dnk273@gmail.com>
2017-09-20 07:10:37 +0300

    20-09-2017

---
+++ lesson408GoFish/Card.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson408GoFish {
+    class Card
+    {
+        public Card(Suits suit, Values value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+        public string Name
+        {
+            get { return $"{Value} of {Suit}"; }
+
+        }
+
+    }
+    enum Suits
+    {
+        Spades = 0,
+        Clubs = 1,
+        Diamonds = 2,
+        Hearts = 3
+    }
+
+    enum Values
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+    }
+
+}
---
+++ lesson408GoFish/Deck.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson408GoFish {
+    class Deck {
+        public Card Peek(int cardNumber) {
+            return cards[cardNumber];
+        }
+        public Card Deal() {
+            returnDeal(0);
+        }
+        public bool ContainsValue(Values value) {
+            foreach (Card card in cards)
+                if (card.Value == value)
+                    return true;
+            return false;
+        }
+        public Deck PullOutValues(Values value) {
+            Deck deckToReturn = new Deck(new Card[] { });
+            for (int i = cards.Count - 1; i >= 0; i--)
+                if (cards[i].Value == value)
+                    deckToReturn.Add(Deal(i));
+            return deckToReturn;
+        }
+        public bool HasBook(Values value) {
+            int NumberOfCards = 0;
+            foreach (Card card in cards)
+                if (card.Value == value)
+                    NumberOfCards++;
+            if (NumberOfCards == 4)
+                return true;
+            else return false;
+        }
+        public void SortByValue() {
+            cards.Sort(new CardComparer_byValue());
+        }
+    }
+}
